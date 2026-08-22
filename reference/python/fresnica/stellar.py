964536e57c6ed58e7074da877445b131d47b3f91
Eno <appweb.cn@gmail.com>
2026-08-22 15:49:13 +0800

    Add Stellar SDK address helper

--- reference/python/fresnica/stellar.py
+++ reference/python/fresnica/stellar.py
@@ -1,5 +1,16 @@
-"""Stellar address and transaction helpers."""
+"""Stellar helpers.
+
+Delegates protocol details to stellar-sdk.
+"""
+
+from stellar_sdk import Keypair
 
 
 def encode_address(public_key: bytes) -> str:
-    raise NotImplementedError
+    """Encode a raw public key into Stellar G address."""
+    return Keypair.from_public_key(public_key).public_key
+
+
+def address_from_keypair(keypair: Keypair) -> str:
+    """Return Stellar account address."""
+    return keypair.public_key
