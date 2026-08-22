26c83f6193aef54c98f764588291ec8b484bbb26
Eno <appweb.cn@gmail.com>
2026-08-22 17:19:17 +0800

    Add Rich CLI renderer abstraction

---
+++ reference/python/fresnica/cli/rich_renderer.py
@@ -0,0 +1,15 @@
+"""Rich based rendering helpers.
+
+CLI presentation is separated from wallet services.
+"""
+
+
+class RichRenderer:
+    def render_balance(self, balances):
+        return balances
+
+    def render_review(self, review):
+        return review
+
+    def render_result(self, result):
+        return result
