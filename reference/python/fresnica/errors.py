a33fbc7ffbd79205a86ed950aa2c1e52ce3bfae9
Eno <appweb.cn@gmail.com>
2026-08-24 00:50:32 +0800

    Restore balance error fields

--- reference/python/fresnica/errors.py
+++ reference/python/fresnica/errors.py
@@ -102,6 +102,8 @@ class TrustlineConfirmationRequired(TransactionError):
 class InsufficientBalanceError(TransactionError):
     def __init__(self, asset: str, requested, available):
         self.asset = asset
+        self.requested = requested
+        self.available = available
         super().__init__(
             f"Insufficient {asset} balance: requested {requested}, available {available}"
         )
